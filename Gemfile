b79c8461074f01519553116d941e7719a9ffccaa
Tia Stutson <tmstutson@gmail.com>
2019-11-05 04:08:26 +0000

    added the ability to change positioning by drag and drop with JS code

--- Gemfile
+++ Gemfile
@@ -72,6 +72,7 @@ gem "mini_magick"
 gem 'carrierwave-aws'
 gem 'stripe'
 gem 'ranked-model'
+gem 'jquery-ui-rails'
 
 
 source 'https://rails-assets.org' do
--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::LessonsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_lesson, only: [:update]
 
 
 	def new
@@ -12,20 +13,35 @@ class Instructor::LessonsController < ApplicationController
     	 redirect_to instructor_course_path(current_section.course)
   	end 
 
+    def update
+      current_lesson.update_attributes(lesson_params)
+      render plain: 'updated!'
+    end
+
 
   private
+    def require_authorized_for_current_lesson
+      if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+      end
+    end
+  
 
-  def require_authorized_for_current_section
-  	if current_course.user != current_user
+    def current_lesson
+      @current_lesson ||=Lesson.find(params[:id])
+    end
+
+    def require_authorized_for_current_section
+    	if current_.user != current_user
   		return render plain: 'Unauthorized', status: :unauthorized
-  	end
-  end
+    	end
+    end
 
     helper_method :current_section
   	def current_section
   		@current_section ||= Section.find(params[:section_id])
 
 	def lesson_params
-	   params.require(:lesson).permit(:title, :subtitle, :video)
+	   params.require(:lesson).permit(:title, :subtitle, :video, :row_order_positions)
 	end
 end
