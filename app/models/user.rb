52f2397af34051d026d600fd70322ba83bcf79b3
Tia Stutson <tmstutson@gmail.com>
2020-01-30 23:09:05 +0000

    most recent change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 	has_many :courses
+  has_many :sections, through: :courses
 	has_many :enrollments
 	has_many :enrolled_courses, through: :enrollments, source: :course
 
-	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
